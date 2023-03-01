078ec063a3f8d68b6b6912f4f6f7595922cdabc3
gebreGL <gebremedin@gmail.com>
2023-03-01 22:44:30 +0100

    u.u

--- misitio/models.py
+++ misitio/models.py
@@ -11,7 +11,7 @@ from django.db import models
 #     movil = models.CharField(max_length=14, blank = False, null = True)
 
 class Concierto(models.Model):
-    nombre = models.CharField(max_length=20, primary_key = True)
+    nombre = models.CharField(max_length=100, primary_key = True)
     fecha = models.DateField(blank = False, null = False)
     lugar = models.CharField(max_length=150, blank = False, null = False)
     precio = models.CharField(max_length=14, blank = False, null = False)
